ebcd80b5accbf7e2a5ae568c9c157a7a880411a8
Aryslan Yakshibaev <Aryslan.Yakshibaev@kaspersky.com>
2024-07-17 16:29:00 +0300

    ui changes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -6,4 +6,14 @@ return {
       prefix = "",
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.filesystem.filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = true, -- You can set this to false if you want to see gitignored files as well
+        hide_hidden = false, -- You might want to set this to false to see hidden files (files starting with .)
+      }
+    end,
+  },
 }
